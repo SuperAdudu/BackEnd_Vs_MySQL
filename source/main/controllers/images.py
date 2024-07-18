571777834d6dc1365c20b19d0eb2b6cd8831ab8d
SuperAdudu <124807398+SuperAdudu@users.noreply.github.com>
2024-07-18 10:05:48 +0700

    commit f2

--- source/main/controllers/images.py
+++ source/main/controllers/images.py
@@ -4,6 +4,6 @@ def register_routes_images(app):
 
     app.add_url_rule('/api/images/add_image',methods=["POST"],view_func=addImage)
 
-    app.add_url_rule('/media/<filename>',view_func=viewImage)
+    app.add_url_rule('/media/<idolName>/<albumName>/<fileName>',view_func=viewImage)
 
     app.add_url_rule('/api/images/get_image_by_album',methods=["GET"],view_func=getImageByAlbum)
--- source/main/functions/images.py
+++ source/main/functions/images.py
@@ -1,9 +1,11 @@
 from source.main.models.images import Images
+from source.main.models.albums import Albums
+from source.main.models.idols import Idols
 from source import db
 from flask import request, jsonify,send_from_directory
 import os
 from werkzeug.utils import secure_filename
-
+import uuid
 
 UPLOAD_FOLDER = os.path.abspath('media')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
@@ -15,27 +17,45 @@ def allowed_file(filename):
 def addImage():
     try:
         data = request.form
-        file = request.files['image'] if 'image' in request.files else None
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            image_url = f"{BASE_URL}/media/{filename}"
+        album = Albums.query.filter(Albums.id == data['idAlbum']).first()
+        idol = Idols.query.filter(Idols.id==data['idModel']).first()
+        files = request.files.getlist('images') 
+        if not files:
+            return jsonify({"status": 400, "message": "No files provided"})
+        if album and idol:
+            for file in files:
+                if file and allowed_file(file.filename):
+
+                    file_extension = file.filename.rsplit('.', 1)[1].lower()
+                    new_filename = f"{uuid.uuid4().hex}.{file_extension}"
+
+                    idol_folder = os.path.join(UPLOAD_FOLDER, idol.name)
+                    album_folder = os.path.join(idol_folder, album.name)
+
+                    os.makedirs(album_folder, exist_ok=True)
+
+                    file_path = os.path.join(album_folder, new_filename)
+                    file.save(file_path)
+                    image_url = f"{BASE_URL}/media/{idol.name}/{album.name}/{new_filename}"
+                else:
+                    image_url = None
+                new_image = Images(
+                    idModel=data['idModel'],
+                    idAlbum=data['idAlbum'],
+                    link=image_url
+                )
+                db.session.add(new_image)
+            db.session.commit()
+            return jsonify({"status":200,"message":"Add image successfull"})
         else:
-            image_url = None
-        new_image = Images(
-            idModel=data['idModel'],
-            idAlbum=data['idAlbum'],
-            link=image_url
-        )
-        db.session.add(new_image)
-        db.session.commit()
-        return jsonify({"status":200,"message":"Add image successfull"})
+            return jsonify({"status":400,"message":"One of idInput is not correct"})    
     except Exception as e:
         return jsonify({"status":404,"message":str(e)})
     
-def viewImage(filename):
+def viewImage(fileName,idolName,albumName):
     try:
-        return send_from_directory(UPLOAD_FOLDER, filename)
+        folder = os.path.join(UPLOAD_FOLDER, idolName, albumName)
+        return send_from_directory(folder, fileName)
     except Exception as e:
         return jsonify({"status":404,"message":str(e)})
     
